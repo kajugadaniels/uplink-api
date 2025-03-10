03c361572f527e744b5e2d1cec90838b35c5d81e
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-11 01:00:07 +0200

    Create Category model in the base app with fields 'name' and 'slug'. The save() method is overridden to automatically generate and update the slug using django.utils.text.slugify whenever the name is modified, ensuring the slug reflects the current category name.

--- base/models.py
+++ base/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+class Category(models.Model):
+    """
+    Represents a category with a human-readable name and an automatically generated slug.
+
+    Attributes:
+        name (str): The name of the category.
+        slug (str): A URL-friendly version of the category name, generated automatically.
+    """
+    name = models.CharField(max_length=255, unique=True, help_text="Enter the name of the category.")
+    slug = models.SlugField(max_length=255, unique=True, editable=False, help_text="Automatically generated from the category name.")
+
+    def save(self, *args, **kwargs):
+        """
+        Overrides the default save method to automatically generate/update the slug from the name.
+        When the category name is changed, the slug is regenerated using the slugify function.
+        """
+        self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """
+        Returns a string representation of the category.
+        """
+        return self.name
