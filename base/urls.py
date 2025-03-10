0e7bb28bd399fa3144748a08bfb6baa25b44a385
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-11 01:20:05 +0200

    Add category routes

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,9 @@ from django.conf.urls.static import static
 app_name = 'base'
 
 urlpatterns = [
-    # 
+    path('categories/', GetCategories.as_view(), name='GetCategories'),
+    path('categories/add/', AddCategory.as_view(), name='AddCategory'),
+    path('categories/<slug:slug>/', CategoryDetails.as_view(), name='CategoryDetails'),
+    path('categories/<slug:slug>/update/', UpdateCategory.as_view(), name='UpdateCategory'),
+    path('categories/<slug:slug>/delete/', DeleteCategory.as_view(), name='DeleteCategory'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
