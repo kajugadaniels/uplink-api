b503f886b8a44f09e2a8dca9050138e33797390f
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-11 19:14:29 +0200

    Create PostImageInline for mobile admin. This inline interface enables managing PostImage instances directly within the Post admin view, allowing for easy addition and modification of images for a post.

--- mobile/admin.py
+++ mobile/admin.py
@@ -1,3 +1,12 @@
+from mobile.models import *
 from django.contrib import admin
 
-# Register your models here.
+class PostImageInline(admin.TabularInline):
+    """
+    Inline admin interface for PostImage.
+    This allows administrators to add or edit images directly from the Post admin page.
+    """
+    model = PostImage
+    extra = 1
+    fields = ('image', 'created_at',)
+    readonly_fields = ('created_at',)
