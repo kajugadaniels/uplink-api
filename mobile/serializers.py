17b6c381b9f6e6e8a024be5d02ae106deb03b8ae
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 00:04:36 +0200

    Override PostSerializer.create to support nested creation of PostImage instances.

--- mobile/serializers.py
+++ mobile/serializers.py
@@ -11,3 +11,27 @@ class PostImageSerializer(serializers.ModelSerializer):
         model = PostImage
         fields = ('id', 'image', 'created_at')
         read_only_fields = ('id', 'created_at')
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Post model.
+    
+    This serializer includes a nested representation of associated post images,
+    allowing the creation and update of a post along with multiple images.
+    """
+    images = PostImageSerializer(many=True, required=False)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'user', 'title', 'category', 'description', 'created_at', 'updated_at', 'images')
+        read_only_fields = ('id', 'created_at', 'updated_at')
+    
+    def create(self, validated_data):
+        """
+        Create a new Post instance along with any associated PostImage instances if provided.
+        """
+        images_data = validated_data.pop('images', [])
+        post = Post.objects.create(**validated_data)
+        for image_data in images_data:
+            PostImage.objects.create(post=post, **image_data)
+        return post
