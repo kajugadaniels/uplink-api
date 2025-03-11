6880c81fb42d0df40239f236b141bf14e81e1623
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-11 18:57:13 +0200

    Create Post model with fields: title, category, description, created_at, and updated_at.

--- mobile/models.py
+++ mobile/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from base.models import *
+from django.utils.text import slugify
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255, help_text="Enter the title of the post.")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts', help_text="Select the category for the post.")
+    description = models.TextField(help_text="Enter the content or description of the post.")
+    created_at = models.DateTimeField(auto_now_add=True, help_text="The date and time when the post was created.")
+    updated_at = models.DateTimeField(auto_now=True, help_text="The date and time when the post was last updated.")
+
+    def __str__(self):
+        """
+        Returns a string representation of the Post.
+        """
+        return self.title
