af1dd18193bab1df823294aee058d235ce99b57c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 00:05:01 +0200

    Override PostSerializer.update to support nested updating of PostImage instances.

--- mobile/serializers.py
+++ mobile/serializers.py
@@ -35,3 +35,20 @@ class PostSerializer(serializers.ModelSerializer):
         for image_data in images_data:
             PostImage.objects.create(post=post, **image_data)
         return post
+
+    def update(self, instance, validated_data):
+        """
+        Update an existing Post instance. If new post images data is provided,
+        delete all existing images and create new PostImage instances.
+        """
+        images_data = validated_data.pop('images', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if images_data is not None:
+            # Delete all existing images before adding new ones
+            instance.images.all().delete()
+            for image_data in images_data:
+                PostImage.objects.create(post=instance, **image_data)
+        return instance
