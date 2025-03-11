517e373b85428f4602110c1732cfddc74ae9b2ee
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-11 19:09:44 +0200

    Update post_image_path to include a 7-digit random number in the file name for each uploaded image, ensuring uniqueness.

--- mobile/models.py
+++ mobile/models.py
@@ -1,41 +1,16 @@
 import os
+import random
 from base.models import *
 from django.db import models
 from django.utils.text import slugify
 from imagekit.processors import ResizeToFill
 from imagekit.models import ProcessedImageField
 
-class Post(models.Model):
-    title = models.CharField(max_length=255, help_text="Enter the title of the post.")
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts', help_text="Select the category for the post.")
-    description = models.TextField(help_text="Enter the content or description of the post.")
-    created_at = models.DateTimeField(auto_now_add=True, help_text="The date and time when the post was created.")
-    updated_at = models.DateTimeField(auto_now=True, help_text="The date and time when the post was last updated.")
-
-    def __str__(self):
-        """
-        Returns a string representation of the Post.
-        """
-        return self.title
-
 def post_image_path(instance, filename):
+    """
+    Generate a unique file path for a post image by appending 7 random digits 
+    to the file name based on the post title.
+    """
     base_filename, file_extension = os.path.splitext(filename)
-    return f'posts/post_{slugify(instance.post.title)}{file_extension}'
-
-class PostImage(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images', help_text="The post to which this image is associated.")
-    image = ProcessedImageField(
-        upload_to=post_image_path,
-        processors=[ResizeToFill(1270, 1270)],
-        format='JPEG',
-        options={'quality': 90},
-        null=True,
-        blank=True
-    )
-    created_at = models.DateTimeField(auto_now_add=True, help_text="The date and time when the image was uploaded.")
-
-    def __str__(self):
-        """
-        Returns a string representation of the PostImage.
-        """
-        return f"Image for {self.post.title}"
+    random_number = random.randint(1000000, 9999999)  # 7-digit random number
+    return f'posts/post_{slugify(instance.post.title)}_{random_number}{file_extension}'
