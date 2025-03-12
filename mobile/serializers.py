ea8dc41d74a7d7fdf306c4e807cca940e12355cd
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 23:36:35 +0200

    New changes

--- mobile/serializers.py
+++ mobile/serializers.py
@@ -18,13 +18,16 @@ class UserSerializer(serializers.ModelSerializer):
 class CategorySerializer(serializers.ModelSerializer):
     """
     Serializer for the Category model.
-    Serializes the 'name' field provided by the user and the auto-generated 'slug' field.
+    
+    Serializes the 'name' field provided by the user and the auto-generated 'slug' field,
+    and includes a nested list of posts associated with the category.
     """
+    posts = PostSerializer(many=True, read_only=True)
+
     class Meta:
         model = Category
-        fields = ['name', 'slug']
+        fields = ['name', 'slug', 'posts']
         read_only_fields = ['slug']
-
 
 class PostImageSerializer(serializers.ModelSerializer):
     """
