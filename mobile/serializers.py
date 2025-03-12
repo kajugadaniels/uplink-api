16a792a87a4f66bf6f9aad972ae37690f5031e35
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 23:22:27 +0200

    Enhance PostImageSerializer to return an absolute URL for the image using SerializerMethodField.

--- mobile/serializers.py
+++ mobile/serializers.py
@@ -14,3 +14,25 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('name', 'email', 'phone_number', 'username', 'image')
+
+class PostImageSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the PostImage model.
+    
+    Overrides the default image field serialization to return the absolute URL of the image.
+    """
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = PostImage
+        fields = ('id', 'image', 'created_at')
+        read_only_fields = ('id', 'created_at')
+
+    def get_image(self, obj):
+        """
+        Returns the absolute URL of the image using the request context.
+        """
+        request = self.context.get('request')
+        if request:
+            return request.build_absolute_uri(obj.image.url)
+        return obj.image.url
