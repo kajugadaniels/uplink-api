f017b06d8e2c8b14856032a253b51f28d7ac755c
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 23:29:28 +0200

    Update CategorySerializer to include a nested 'posts' field using PostSerializer. This change retrieves all posts associated with each category via the related_name 'posts', providing detailed category and post data in a single response.

--- base/serializers.py
+++ base/serializers.py
@@ -1,12 +1,17 @@
 from base.models import *
+from mobile.serializers import *
 from rest_framework import serializers
 
 class CategorySerializer(serializers.ModelSerializer):
     """
     Serializer for the Category model.
-    Serializes the 'name' field provided by the user and the auto-generated 'slug' field.
+    
+    Serializes the 'name' field provided by the user and the auto-generated 'slug' field,
+    and includes a nested list of posts associated with the category.
     """
+    posts = PostSerializer(many=True, read_only=True)
+
     class Meta:
         model = Category
-        fields = ['name', 'slug']
+        fields = ['name', 'slug', 'posts']
         read_only_fields = ['slug']
