9051c2844afe4ef59790988bb1cea30bbbaffdc1
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 23:46:37 +0200

    Fix erros

--- base/serializers.py
+++ base/serializers.py
@@ -1,12 +1,17 @@
 from base.models import *
+from mobile.serializers import *
 from rest_framework import serializers
 
 class CategorySerializer(serializers.ModelSerializer):
     """
     Serializer for the Category model.
-    Serializes the 'name' field provided by the user and the auto-generated 'slug' field.
+    
+    Serializes the 'name' and auto-generated 'slug' fields.
+    Also includes a nested list of posts (using PostSerializer) associated with the category.
     """
+    posts = PostSerializer(many=True, read_only=True, context={'request': None})
+
     class Meta:
         model = Category
-        fields = ['name', 'slug']
+        fields = ['name', 'slug', 'posts']
         read_only_fields = ['slug']
