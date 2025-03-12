07a9c11d0f3df44194ec02b4f41e15080fd3f1dc
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-12 22:35:34 +0200

    Implement UserSerializer to include detailed user info for nested representation in PostSerializer.

--- mobile/serializers.py
+++ mobile/serializers.py
@@ -2,6 +2,9 @@ from mobile.models import *
 from account.models import *
 from base.serializers import *
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     """
