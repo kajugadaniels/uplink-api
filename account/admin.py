0a7c236f8b5a3913e49514e4d78e1abcf37105c5
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-21 16:31:36 +0200

    Register User model in admin with custom actions for edit and delete, enhancing usability

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'email',
+        'username',
+        'phone_number',
+        'created_at',
+        'actions'
+    )
+    search_fields = ('name', 'email', 'username', 'phone_number')
+    list_filter = ('is_active', 'is_staff')
+    ordering = ('created_at',)
+
+    def actions(self, obj):
+        change_url = reverse("admin:account_user_change", args=[obj.pk])
+        delete_url = reverse("admin:account_user_delete", args=[obj.pk])
+        return format_html(
+            '<a class="button" style="margin-right: 8px;" href="{}">Edit</a>'
+            '<a class="button" style="color: red;" href="{}">Delete</a>',
+            change_url,
+            delete_url
+        )
+    actions.short_description = "Actions"
