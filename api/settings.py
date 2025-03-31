b8f1cca18ce963f67905f2534ce5085f1423f714
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-31 19:09:40 +0200

    New changes

--- api/settings.py
+++ api/settings.py
@@ -83,39 +83,38 @@ WSGI_APPLICATION = 'api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# if str(os.getenv("NODE_ENV"))=="production":
-
-#     DATABASES =  {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.getenv("POSTGRES_DB"),      
-#             'USER': os.getenv("POSTGRES_USER"),       
-#             'PASSWORD': os.getenv("POSTGRES_PASSWORD"), 
-#             'HOST': os.getenv("PGHOST"),         
-#             'PORT':os.getenv("PGPORT"), 
-#             'OPTIONS': {
-#                 'sslmode': 'require',
-#             }            
-#         }
-#     }
-# else:
-#     DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv("MYSQL_DB"),
-#         'USER': os.getenv("MYSQL_USER"),
-#         'PASSWORD': os.getenv("MYSQL_PASSWORD"),
-#         'HOST': os.getenv("MYSQL_HOST"),
-#         'PORT': os.getenv("MYSQL_PORT"),
-#     }
-# }
-
-DATABASES = {
+if str(os.getenv("NODE_ENV"))=="production":
+    DATABASES =  {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("POSTGRES_DB"),      
+            'USER': os.getenv("POSTGRES_USER"),       
+            'PASSWORD': os.getenv("POSTGRES_PASSWORD"), 
+            'HOST': os.getenv("PGHOST"),         
+            'PORT':os.getenv("PGPORT"), 
+            'OPTIONS': {
+                'sslmode': 'require',
+            }            
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("MYSQL_DB"),
+        'USER': os.getenv("MYSQL_USER"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD"),
+        'HOST': os.getenv("MYSQL_HOST"),
+        'PORT': os.getenv("MYSQL_PORT"),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
